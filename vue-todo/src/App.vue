38a7ba7b1e43ca1f58a6d868706bc75224a814b4
kyafuso <kazuya.yafuso.0825@gmail.com>
2021-12-10 19:55:31 +0900

    課題③-1 タスク追加機能

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>TODOリスト</h1>
+    <ToDoList />
+    <ToDoForm />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ToDoList from './components/ToDoList.vue'
+import ToDoForm from './components/ToDoForm.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ToDoList,
+    ToDoForm
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- vue-todo/src/store/index.js
+++ vue-todo/src/store/index.js
@@ -5,8 +5,24 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    todos : []
+  },
+  getters: {
+    todos: state => {
+      return state.todos
+    }
   },
   mutations: {
+    ADD_TODO (state, comment) {
+      
+      const id = state.todos.length;
+
+      state.todos.push({
+        id: id,
+        comment: comment,
+        status: '作業中'
+      });
+    }
   },
   actions: {
   },
